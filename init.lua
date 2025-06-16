4554d9361f019a8068760b9d3dee72a1ae5b0afd
Irfan Muhammad Fauzi <irfanmg96@gmail.com>
2025-06-16 16:13:29 +0700

    refactor general_remap

--- init.lua
+++ init.lua
@@ -285,6 +285,35 @@ end, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+local relativenumber = true
+vim.opt.relativenumber = relativenumber
+vim.opt.nu = true
+vim.keymap.set('n', '<leader>trn', function()
+  vim.opt.relativenumber = not relativenumber
+  relativenumber = not relativenumber
+end, { desc = 'togle relative number' })
+vim.opt.swapfile = false
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Jump up half page and set cursor to mid' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Jump down half page and set cursor to mid' })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.fn.expand('%:p')
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without replacing clipboard' })
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Open Git Status' })
+
+
+return {}
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
